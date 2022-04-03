4428c6aa169255e479f8fc7df22288d36472551d
Brena Paiva <brenaennet@gmail.com>
2022-04-03 01:04:38 -0300

    Inicio do projeto

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
+import { ensureAutheticate } from "./middleware/ensureAuthenticate";
+
+const router = Router();
+
+router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post("/messages", ensureAutheticate, new CreateMessageController().handle)
+
+router.get("/messages/last3", new GetLast3MessagesController().handle)
+router.get("/profile", ensureAutheticate, new ProfileUserController().handle)
+
+export { router };
---
+++ src/server.ts
@@ -0,0 +1,4 @@
+import { serverHttp } from "./app"
+
+serverHttp.listen(4000, () =>
+ console.log(` 🚀🚀 Server is running on PORT 4000 🚀🚀`));
